b60b8f16dbb714128adc1dbf365fd57e9ab7c359
VAGHASHIYA FALGUN <87703871+lordgaruda@users.noreply.github.com>
2024-01-10 17:59:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 rm -rf /cache/*
 
 BASE_DIR="$(pwd)"
-SOURCEDIR="${BASE_DIR}/awaken"
+SOURCEDIR="${BASE_DIR}/pixelos"
 
-git config --global user.email "omrameshpatel@gmail.com" && git config --global user.name "takumi021"
+git config --global user.email "falgunpatel.fv@proton.me" && git config --global user.name "falgunv"
 df -h
 mkdir -p "${SOURCEDIR}"
 cd "${SOURCEDIR}"
@@ -17,26 +17,26 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
 chmod a+rx ~/.bin/repo
 
 # Sync Source
-repo init -u https://github.com/Project-Awaken/android_manifest -b triton --depth=1
+repo init -u repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen --depth=1
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # Sync Trees
-git clone  https://github.com/realme-sm6125-devs/device_realme_r5x device/realme/r5x -b 13
-git clone  https://github.com/realme-sm6125-devs/vendor_realme_r5x vendor/realme/r5x -b thirteen
-git clone  https://github.com/mcdofrenchfreis/biofrost-kernel-realme-trinket -b inline/dynamic kernel/realme/r5x
+git clone  https://github.com/PixelOS-Devices/device_realme_r5x device/realme/r5x -b thirteen
+git clone  https://github.com/PixelOS-Devices/vendor_realme_r5x vendor/realme/r5x -b thirteen
+git clone  https://github.com/takumi021/kernel_realme_trinket -b inline kernel/realme/r5x
 
 # Source Patches
-cd frameworks/base
-wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices -a12l.patch
-patch -p1 <*.patch
-cd "${SOURCEDIR}"
+#cd frameworks/base
+#wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices -a12l.patch
+#patch -p1 <*.patch
+#cd "${SOURCEDIR}"
 
 # Start Build
 source build/envsetup.sh
-lunch awaken_r5x-userdebug
+lunch aosp_r5x-userdebug
 make bacon -j$(nproc --all)
 
 # Upload
 cd out/target/product/r5x
 curl -sL https://git.io/file-transfer | sh
-./transfer wet awaken*.zip
+./transfer wet pixelos*.zip
