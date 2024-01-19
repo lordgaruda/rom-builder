abc67b650da597fc4f7b15464f524402ef311f08
FALGUN VAGHSHIYA <87703871+lordgaruda@users.noreply.github.com>
2024-01-19 08:51:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 rm -rf /cache/*
 
 BASE_DIR="$(pwd)"
-SOURCEDIR="${BASE_DIR}/pixelos"
+SOURCEDIR="${BASE_DIR}/awaken"
 
-git config --global user.email "falgunpatel.fv@proton.me" && git config --global user.name "falgunv"
+git config --global user.email "falgunpatel.fv@gmail.com" && git config --global user.name "falgunv"
 df -h
 mkdir -p "${SOURCEDIR}"
 cd "${SOURCEDIR}"
@@ -17,12 +17,12 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
 chmod a+rx ~/.bin/repo
 
 # Sync Source
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen --depth=1
+repo init -u https://github.com/Project-Awaken/android_manifest -b triton --depth=1
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # Sync Trees
-git clone  https://github.com/PixelOS-Devices/device_realme_r5x device/realme/r5x -b thirteen
-git clone  https://github.com/PixelOS-Devices/vendor_realme_r5x vendor/realme/r5x -b thirteen
+git clone  https://github.com/realme-sm6125-devs/device_realme_r5x device/realme/r5x -b 13
+git clone  https://github.com/realme-sm6125-devs/vendor_realme_r5x vendor/realme/r5x -b thirteen
 git clone  https://github.com/takumi021/kernel_realme_trinket -b inline kernel/realme/r5x
 
 # Source Patches
@@ -33,10 +33,10 @@ git clone  https://github.com/takumi021/kernel_realme_trinket -b inline kernel/r
 
 # Start Build
 source build/envsetup.sh
-lunch aosp_r5x-userdebug
+lunch awaken_r5x-userdebug
 make bacon -j$(nproc --all)
 
 # Upload
 cd out/target/product/r5x
 curl -sL https://git.io/file-transfer | sh
-./transfer wet pixelos*.zip
+./transfer wet awaken*.zip
